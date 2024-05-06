22002dc0b92c002b72b928dec2bc0087a57d383e
Irvin Amilcar <iafbs@cesar.school>
2024-05-06 15:47:07 -0300

    inicio do projeto com a parte escreita do menu interativo e a primeira função adicionar.

---
+++ la.py
@@ -0,0 +1,33 @@
+def adicionar ():
+    open()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    print("""
+    Adicionar receitas  (1)
+    Remover receitas    (2)
+    Visualizar receitas (3)
+    Atualizar receitas  (4)
+    Filtras por país    (5)
+    Lista de favoritos  (6)
+    Receita aleatória   (7)
+    (função especial)   (8)
+    Sair                (0)
+    """)
