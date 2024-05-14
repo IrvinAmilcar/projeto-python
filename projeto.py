d912e197e10f3fd324d78e7cfa5a7794991b11dd
Irvin Amilcar <iafbs@cesar.school>
2024-05-14 10:38:37 -0300

    base

--- projeto.py
+++ projeto.py
@@ -1,2 +1,23 @@
+import os
 
+print("Iniciar programa? (start)")
+inicializador = input()
+if inicializador == "start":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("""
+    MENU___________________
 
+    (1) Adicionar receitas
+    (2) Visualisar receitas
+    (3) Atualizar receitas
+    (4) Remover receitas
+    (5) Sair
+    """)
+    
+    escolha = int(input("Escolha uma opção: "))
+   
+    if escolha == 1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("""
+        Opção selecionada: Adicionar
+        """)
