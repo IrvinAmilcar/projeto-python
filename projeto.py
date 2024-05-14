33711511148f0ca88f4a517c9e0732165c338259
Irvin Amilcar <iafbs@cesar.school>
2024-05-14 15:10:01 -0300

    Atualização principal

--- projeto.py
+++ projeto.py
@@ -1,25 +1,74 @@
 import os
 
-print("Iniciar programa? (start)")
-inicializador = input()
-if inicializador == "start":
-    os.system('cls' if os.name == 'nt' else 'clear')
+def menu_principal():
     print("""
-    MENU___________________
 
-    (1) Adicionar receitas
-    (2) Visualisar receitas
+    Menu Principal______________
+
+    (1) Cadastrar novas receitas
+    (2) Consultar receitas
     (3) Atualizar receitas
-    (4) Remover receitas
-    (5) Sair
+    (4) Excluir receitas 
+    (5) Sugerir uma receita
+    (6) Sair
     """)
-    
-    escolha = int(input("Escolha uma opção: "))
-   
-    if escolha == 1:
+    opcao = int(input("Digite a opção desejada: "))
+    if opcao == 1:
+        Cadastrar_Receitas()
+    elif opcao == 2:
+        Visualizar_receitas()
+    else:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("""
-        Opção selecionada: Adicionar
-        """)
-        while True:
-            escolha ==
+        print("Opção inválida")
+        print("__PROGRAMA ENCERRADO__")
+
+def Cadastrar_Receitas():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Opção selecionada: Cadastros de receitas!")
+    while True:
+        nome_receita = input("Digite o nome da receita: ")
+        pais_de_origem = input("Digite o país de origem da receita: ")
+        ingredientes = input("Digite os ingredientes da receita (separados por vírgula): ")
+        preparo = input("Digite o modo de preparo da receita: ")
+
+        with open("Receitas.txt", "a", encoding="utf-8") as f:
+            f.write(f"Receita: {nome_receita}\n")
+            f.write(f"País de origem: {pais_de_origem}\n")
+            f.write(f"Ingredientes: {ingredientes}\n")
+            f.write(f"Modo de preparo: {preparo}\n\n")
+
+        print("Receita adicionada com sucesso!")
+        continuar = input("Deseja cadastrar outra receita? (s/n): ")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if continuar.lower()!= "s":
+            
+            print("Deseja voltar ao menu (1) ou encerrar o programa (2)?")
+            escolha_c = int(input())
+            if escolha_c == 1:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                menu_principal()
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("Programa ENCERRADO!")
+                break
+
+def Visualizar_receitas():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Opção selecionada: Visualizar receitas!")
+
+
+
+    
+
+
+
+
+print("Deseja iniciar o programa? (start)")
+
+inicializador = input()
+os.system('cls' if os.name == 'nt' else 'clear')
+
+if inicializador == 'start':
+    menu_principal()
+else:
+    print("O programa nem iniciou e ja encerrou")
