bcf37041d2cc6e0ab27b091f3fbe8898e3576b98
Irvin Amilcar <iafbs@cesar.school>
2024-05-16 08:29:39 -0300

    atualização principal, função visualizar (irvin)

--- projeto.py
+++ projeto.py
@@ -56,6 +56,66 @@ def Cadastrar_Receitas():
 def Visualizar_receitas():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Opção selecionada: Visualizar receitas!")
+    print("\nVisualizar todas as receitas (1)")
+    print("Visualizar receitas por nome (2)\n")
+    escolha_v = (input("Escolha uma operação ou 'sair' para voltar ao menu: "))
+    if escolha_v == 'sair':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        menu_principal()
+    elif escolha_v == '1':
+        with open("Receitas.txt", "r", encoding="utf-8") as f:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            for linha in f:
+                print(linha.strip())
+        escolha_v1 = int(input("Deseja voltar ao menu (1) ou encerrar o programa (2): "))
+        if escolha_v1 == 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            menu_principal()
+        elif escolha_v1 == 2:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Programa ENCERRADO!")
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Opção inválida!")
+            print("Programa ENCERRADO")
+    elif escolha_v == '2':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        palavra_chave = input("Informe o nome da receita: ")
+        try:
+            with open("Receitas.txt", "r", encoding="utf-8") as f:
+                linhas = f.readlines()
+                found = False
+                for e in range(len(linhas)):
+                    if palavra_chave.lower() in linhas[e].lower():
+                        found = True
+                        print(linhas[e].strip())
+                        for b in range(e + 1, len(linhas)):
+                            if linhas[b].strip() == "":
+                                break
+                            print(linhas[b].strip())
+                        break
+                if not found:
+                    print("Receita não encontrada!")
+        except FileNotFoundError:
+            print("Arquivo 'Receitas.txt' não encontrado!")
+
+        escolha_v1 = input("Deseja voltar ao menu (1) ou encerrar o programa (2): ")
+        if escolha_v1 == '1':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            menu_principal()
+        elif escolha_v1 == '2':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Programa ENCERRADO!")
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Opção inválida!")
+            print("Programa ENCERRADO")
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Opção inválida!")
+        Visualizar_receitas()
+
+
     
 
     
